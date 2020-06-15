b203d8cc2c8ced5cc02288f716727c674c763b96
Jeremy Gustie <jeremy@carbonrelay.com>
2020-06-15 12:55:25 -0400

    More quiet

--- install-docker-for-mac.sh
+++ install-docker-for-mac.sh
@@ -11,7 +11,7 @@ sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
 sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
 sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
 
-/Applications/Docker.app/Contents/MacOS/Docker --unattended &
+/Applications/Docker.app/Contents/MacOS/Docker --unattended &>/dev/null &
 
 retries=0
 while ! docker info &>/dev/null ; do
@@ -22,7 +22,7 @@ while ! docker info &>/dev/null ; do
         echo 'Docker still running'
     else
         echo 'Docker not running, restart'
-        /Applications/Docker.app/Contents/MacOS/Docker --unattended &
+        /Applications/Docker.app/Contents/MacOS/Docker --unattended &>/dev/null &
     fi
 
     if [[ ${retries} -gt 30 ]]; then
